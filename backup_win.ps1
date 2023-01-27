22e9ef0425f4ccff9926de5611b95d2e54a8092f
Raiden Erdmann <raiden.erdmann@gmail.com>
2023-01-27 20:14:58 +0100

    more structure

--- backup_win.ps1
+++ backup_win.ps1
@@ -1,10 +1,11 @@
 [CmdletBinding()]
 param (
     $backupFile = "Documents.7z",
+    $backupFolder = "backup",
     $password,
-    $mx=9,
-    $mhe="on"
-    # $v="-v100m"
+    [int]$mx = 9,
+    $mhe = "on",
+    [int]$v = 100
 )
 
 
@@ -21,6 +22,18 @@ $toBack += "$PSScriptRoot\backup_info.txt"
 
 
 # Script
+
+if(Test-Path $backupFolder -PathType Container){
+    Remove-Item $backupFolder -Recurse -Force
+}
+foreach ($dir in ("tmp", $backupFolder)){
+    if(!(Test-Path $dir -PathType Container)){
+        New-Item $dir -ItemType Directory -Force
+    }
+}
+
+$backupFile = "$backupFolder\$backupFile"
+
 $x = 'Microsoft\Windows\CurrentVersion\Uninstall\*'
 $backupInfo = ("HKLM:\Software\$x", "HKCU:\SOFTWARE\$x", "HKLM:\Software\Wow6432Node\$x") | Get-ItemProperty | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate -Unique | Sort-Object -Property DisplayName
 $backupInfo | Out-File "$PSScriptRoot\backup_info.txt" -Encoding utf8 -NoNewline
@@ -28,29 +41,34 @@ $backupInfo | Out-File "$PSScriptRoot\backup_info.txt" -Encoding utf8 -NoNewline
 $backupInclude = "$PSScriptRoot\backup_include.txt"
 $backupExclude = "$PSScriptRoot\backup_exclude.txt"
 
-if (Test-Path $backupFile) {
-    Remove-Item $backupFile -ErrorAction Stop
+
+if (Test-Path $backupExclude) {
+    Write-Host "Deleting $backupExclude"
+    Remove-Item $backupExclude -ErrorAction Inquire
+}
+foreach ($folder in $toBack) {
+    if ((Get-Item $folder) -is [IO.FileInfo]) { continue }
+
+    Write-Host "Backing $folder"
+    $folderRel = $folder.LastIndexOf("\") + 1
+    try {
+        foreach ($n in (Get-ChildItem $folder -Recurse -Filter ".nobackup").Directory.FullName) {
+            $nobackupFile += $n.Substring($folderRel) + "`n"
+        }
+    } catch { $_ }
 }
 
-try {
-    if (Test-Path $backupExclude) { $backupExclude | Remove-Item -ErrorAction Inquire }
-    foreach ($folder in $toBack) {
-        if ((Get-Item $folder) -is [IO.FileInfo]) { continue }
-
-        Write-Host "Backing $folder"
-        $folderRel = $folder.LastIndexOf("\") + 1
-        try {
-            foreach ($n in (Get-ChildItem $folder -Recurse -Filter ".nobackup").Directory.FullName) {
-                $nobackupFile += $n.Substring($folderRel) + "`n"
-            }
-        } catch { $_ }
-    }
-
-    # params
-    $nobackupFile | Out-File $backupExclude -Encoding utf8
-    $toBack | Out-File $backupInclude -Encoding utf8
-    . $sevenZip a -t7z -mx=9 -v100m $backupFile -x@"backup_exclude.txt" -i@"backup_include.txt"
-} finally {
-    Write-Host ".nobackup's found in:"
-    Write-Host $nobackupFile
+# params
+$nobackupFile | Out-File $backupExclude -Encoding utf8
+$toBack | Out-File $backupInclude -Encoding utf8
+if ($v) {
+    $split = "-v${v}m"
 }
+. $sevenZip a "-t7z" "-mx=$mx" "$split" "-p""$password""" $backupFile -x@"backup_exclude.txt" -i@"backup_include.txt"
+
+if (!$?) {
+    return
+}
+
+Write-Host ".nobackup's found in:"
+Write-Host $nobackupFile
